cf75b72ab14fd79d24ca7745f131732f9d3da2e7
kienht58 <kien.isp15@gmail.com>
2015-10-12 01:00:10 +0700

    fix some errors

--- dist/js/quiz_view.js
+++ dist/js/quiz_view.js
@@ -3,5 +3,5 @@ var quiz_view = angular.module("quizView", []);
 quiz_view.controller("viewCtrl", function($scope){
     $scope.number = 1;
     $scope.head = "1 + 1 = ?";
-    $scope.choice = "1";
+    $scope.choice = ["1", "2", "3", "4"];
 });
