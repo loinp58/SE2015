8627789accfd58705a0ecfc138c353f2cc26d714
loiprovd193 <nguyenphucloi193@gmail.com>
2015-10-20 01:49:09 +0700

    Revert "add question"
    This reverts commit 0e817a258f57730958047faf8d30801f39b364bb.

--- client/dist/js/quiz_view.js
+++ client/dist/js/quiz_view.js
@@ -1,10 +1,7 @@
 var quiz_view = angular.module("quizView", []);
 
 quiz_view.controller("viewCtrl", function($scope){
-	$scope.username = "Kdz";
-	$scope.questions = [
-		{type: 'multipleChoice', question: "question 1", choice: ["a", "b", "c", "d"], answer: "d"},
-		{type: 'yesNo', question: "question 2", choice: ["yes", "no"], answer: "yes"},
-		{type: 'fill', question: "question 3", choice: "", answer: "answer"}
-	];
+    $scope.number = 1;
+    $scope.head = "1 + 1 = ?";
+    $scope.choice = ["1", "2", "3", "4"];
 });
