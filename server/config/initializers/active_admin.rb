74ac88d4f0f1ea37453962d5b9ad77a66f285d18
kimhung222 <leonkyagami@gmail.com>
2015-11-04 02:04:46 +0700

    add basic api and set root

--- server/config/initializers/active_admin.rb
+++ server/config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Se2015"
-
+  # config.root_to = 'admin/dashboard#index'
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
@@ -112,7 +112,7 @@ ActiveAdmin.setup do |config|
   # roots for each namespace.
   #
   # Default:
-  # config.root_to = 'dashboard#index'
+   config.root_to = 'dashboard#index'
 
   # == Admin Comments
   #
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  api_version(:module => "V1", :path => {:value => "v1"}, :default => true) do
+    resources :question
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
+  root to: "admin/dashboard#index"
   ActiveAdmin.routes(self)
   resources :yesnochoices
   resources :multiplechoices
   get 'multiplechoices/create'
 
   get 'wellcome/index'
-  root 'wellcome#index'
+  #root 'admin/admin/dashboard#index'
+  #root 'wellcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
