c70c42d56e5acfbb78aa255137de688a65e55360
loiprovd193 <nguyenphucloi193@gmail.com>
2015-11-17 21:41:08 +0700

    build client by using components

--- client/dist/js/quiz_view.js
+++
@@ -1,6 +0,0 @@
-var quiz_view = angular.module("quizView", []);
-
-quiz_view.controller("viewCtrl", function($scope, $http){
-    $http.get("http://www.w3schools.com/angular/customers.php")
-    .success(function(response) {$scope.questions = response.questions;});
-});
